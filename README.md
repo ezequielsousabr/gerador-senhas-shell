60a1ed7afae0915bfbdf2902b69d98fcf0790039
Ezequiel Sousa <ezequiel.sousa@yahoo.com>
2022-12-14 02:42:09 -0300

    Gerador de senhas - inicial

---
+++ README.md
@@ -0,0 +1,27 @@
+<p>Gerador de senhas em shell-script <b>bash</b>.</p>
+<hr/>
+                           _                             _               
+                          | |                           | |              
+   __ _  ___ _ __ __ _  __| | ___  _ __   ___  ___ _ __ | |__   __ _ ___ 
+  / _` |/ _ \ '__/ _` |/ _` |/ _ \| '__| / __|/ _ \ '_ \| '_ \ / _` / __|
+ | (_| |  __/ | | (_| | (_| | (_) | |    \__ \  __/ | | | | | | (_| \__ \
+  \__, |\___|_|  \__,_|\__,_|\___/|_|    |___/\___|_| |_|_| |_|\__,_|___/
+   __/ |                                                                 
+  |___/                                                                  
+
+<br/>
+
+<hr/>
+
+<br/>
+
+1. Permissão de execução
+`chmod +x ./gerarsenha.sh`<br/>
+`ls -lh gerarsenha.sh`<br/>
+
+2. Gerar senhas alfanumerico
+`sh gerarsenha.sh`<br/>
+`./gerarsenha.sh`<br/>
+
+3. Informar o tamanho da senha
+> Ex: 100
---
+++ gerarsenha.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+# Autor: Ezequiel Sousa
+# Função para gerar senhas alfanuméricas com Shell Scripiting
+# ./gerarsenha.sh
+# Desde: ter Fev 14 02:13:30 BRT 2022
+# Versão: 1.1.0
+# Licença: GPL
+printf "
+# ----------------------------------------------------------------------------
+                           _                             _               
+                          | |                           | |              
+   __ _  ___ _ __ __ _  __| | ___  _ __   ___  ___ _ __ | |__   __ _ ___ 
+  / _` |/ _ \ '__/ _` |/ _` |/ _ \| '__| / __|/ _ \ '_ \| '_ \ / _` / __|
+ | (_| |  __/ | | (_| | (_| | (_) | |    \__ \  __/ | | | | | | (_| \__ \
+  \__, |\___|_|  \__,_|\__,_|\___/|_|    |___/\___|_| |_|_| |_|\__,_|___/
+   __/ |                                                                 
+  |___/                                                                   
+# ---------------------------------------------------------------------------- \n"
+gerarsenha(){
+ letras='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ numeros='0123456789'
+ lista="$letras$numeros"
+ 
+ printf "Qual o tamanho da senha?(Exemplo: 24)\n"
+ read max
+ 
+ tamanho=${#lista}
+ 
+ for ((i = 0; i < $max; i++ ))
+  do
+   ordem=$((RANDOM % $tamanho + 1))
+   senha=$(echo $lista | cut -c "$ordem")
+   echo -n $senha
+  done
+ echo
+ printf "Senha gerada com sucesso!\n"
+}
+gerarsenha
